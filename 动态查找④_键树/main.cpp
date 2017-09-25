0879f7165ac0f2df3e04211a150773306df474a8
Mike <82643885@qq.com>
2017-09-25 13:34:36 +0800

    no message

---
+++ 动态查找④_键树/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  动态查找④_键树
+//
+//  Created by Mike on 2017/9/25.
+//  Copyright © 2017年 Mike. All rights reserved.
+//
+
+#include <iostream>
+
+/*
+ 键树又称数字查找树(Digital Search Tree)。它是一棵读>=2的树，树中的每个结点中**并不是包含数据元素，而是包含组成数据元素的符号**。例如，若关键字是数值，则结点中只包含一个数位，若关键字是单词，则结点中只包含一个字母字符。这种树会给某种类型关键字的表的查找带来方便。
+ 
+ 
+ 通常，键树有两种存储结构：
+ ①双链树
+ ②以树的多重链表表示键树，则树的每个结点中应含有d个指针域，此时的键树又称Trie树。
+ */
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
